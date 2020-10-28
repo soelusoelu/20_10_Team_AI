b5208a0aaf29840608c97d658b992f06c2bc46a6
TakanoKakeru <llmn.0419@gmail.com>
2020-10-28 12:01:53 +0900

    D3DXからの解放

--- DirectX/System/Texture/TextureFromFile.cpp
+++ DirectX/System/Texture/TextureFromFile.cpp
@@ -12,56 +12,33 @@ TextureFromFile::TextureFromFile(const std::string& filePath) :
 TextureFromFile::~TextureFromFile() = default;
 
 void TextureFromFile::createTextureFromFileName(const std::string& filePath) {
-    //DirectX::TexMetadata metadata{};
-    //DirectX::ScratchImage scratchImage{};
+    //COMの初期化
+    if (FAILED(CoInitializeEx(0, COINIT_MULTITHREADED))) {
+        Debug::windowMessage("TextureFromFileクラスのCOM初期化に失敗しました");
+        return;
+    }
 
-    ////Unicodeへ変換する
-    //wchar_t dst[256];
-    //MultiByteToWideChar(CP_ACP, 0, filePath.c_str(), -1, dst, _countof(dst));
+    DirectX::TexMetadata metadata{};
+    DirectX::ScratchImage scratchImage{};
 
-    ////ファイルからテクスチャ情報を取得
-    //if (FAILED(DirectX::LoadFromWICFile(dst, DirectX::WIC_FLAGS_NONE, &metadata, scratchImage))) {
-    //    Debug::windowMessage(filePath + ": テクスチャ読み込み失敗");
-    //    return;
-    //}
-
-    ////生データ抽出
-    //const DirectX::Image* image = scratchImage.GetImage(0, 0, 0);
-
-    ////テクスチャサイズの設定
-    //mTextureSize = Vector2(metadata.width, metadata.height);
-
-    //ID3D11ShaderResourceView* srv = nullptr;
-    //if (FAILED(DirectX::CreateShaderResourceView(MyDirectX::DirectX::instance().device(), image, 1, metadata, &srv))) {
-    //    Debug::windowMessage(filePath + ": テクスチャ作成失敗");
-    //}
-    //mShaderResourceView = std::make_unique<ShaderResourceView>(srv);
-
-
+    //Unicodeへ変換する
+    wchar_t dst[256];
+    MultiByteToWideChar(CP_ACP, 0, filePath.c_str(), -1, dst, _countof(dst));
 
     //ファイルからテクスチャ情報を取得
-    D3DX11_IMAGE_INFO info{};
-    D3DX11GetImageInfoFromFileA(filePath.c_str(), nullptr, &info, nullptr);
+    if (FAILED(DirectX::LoadFromWICFile(dst, DirectX::WIC_FLAGS_NONE, &metadata, scratchImage))) {
+        Debug::windowMessage(filePath + ": テクスチャ読み込み失敗");
+        return;
+    }
 
-    mTextureSize = Vector2(info.Width, info.Height);
+    //生データ抽出
+    const DirectX::Image* image = scratchImage.GetImage(0, 0, 0);
 
-    D3DX11_IMAGE_LOAD_INFO loadInfo{};
-    loadInfo.Width = info.Width;
-    loadInfo.Height = info.Height;
-    loadInfo.Depth = info.Depth;
-    loadInfo.MipLevels = info.MipLevels;
-    loadInfo.MiscFlags = info.MiscFlags;
-    loadInfo.Format = info.Format;
-    loadInfo.FirstMipLevel = 0;
-    loadInfo.Usage = toUsage(Usage::USAGE_DEFAULT);
-    loadInfo.BindFlags = static_cast<unsigned>(Texture2DBind::TEXTURE_BIND_SHADER_RESOURCE);
-    loadInfo.CpuAccessFlags = 0;
-    loadInfo.Filter = D3DX11_FILTER_LINEAR; //線形補間
-    loadInfo.MipFilter = D3DX11_FILTER_LINEAR; //線形補間
-    loadInfo.pSrcInfo = nullptr;
+    //テクスチャサイズの設定
+    mTextureSize = Vector2(metadata.width, metadata.height);
 
     ID3D11ShaderResourceView* srv = nullptr;
-    if (FAILED(D3DX11CreateShaderResourceViewFromFileA(MyDirectX::DirectX::instance().device(), filePath.c_str(), &loadInfo, nullptr, &srv, nullptr))) {
+    if (FAILED(DirectX::CreateShaderResourceView(MyDirectX::DirectX::instance().device(), image, 1, metadata, &srv))) {
         Debug::windowMessage(filePath + ": テクスチャ作成失敗");
     }
     mShaderResourceView = std::make_unique<ShaderResourceView>(srv);
